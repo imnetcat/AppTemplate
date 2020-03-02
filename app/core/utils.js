fac887cd7126d08f877f516cf3a09a52a370c9c9
imnetcat <guskov.danil@gmail.com>
2020-03-02 13:26:51 -0500

    add partialEx, curry, curryEx

--- app/core/utils.js
+++ app/core/utils.js
@@ -9,10 +9,17 @@ class Utils {
     return x => fncs.reduce((v, f) => f(v), x);
   }
 
-  static partial(fn, ...args) {
+  static partial(fn) {
+    return (...rest) => fn(...rest);
+  }
+  static partialEx(fn, ...args) {
     return (...rest) => fn(...args, ...rest);
   }
 
+  static extend(obj, mixin) {
+    Object.assign(obj, mixin);
+  }
+  
   static extendEx(obj, name, mixin) {
     obj[name] = mixin;
     const keys = Object.keys(mixin);
@@ -22,8 +29,24 @@ class Utils {
     }
   }
 
-  static extend(obj, mixin) {
-    Object.assign(obj, mixin);
+  static curry(fn){
+    return (...args) => {
+      if (fn.length > args.length) {
+        const f = fn.bind(null, ...args);
+        return curry(f);
+      } else {
+        return fn(...args);
+      }
+    }
+  }
+
+  static curryEx(fn, ...par) {
+    const curried = (...args) => (
+      fn.length > args.length ?
+        curry(fn.bind(null, ...args)) :
+        fn(...args)
+    );
+    return par.length ? curried(...par) : curried;
   }
 };
 
