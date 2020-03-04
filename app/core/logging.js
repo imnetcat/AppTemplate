bbf347c13ead8545c64045fb0a742e1807fa4522
imnetcat <guskov.danil@gmail.com>
2020-03-04 15:35:38 -0500

    set static

--- app/core/logging.js
+++ app/core/logging.js
@@ -6,21 +6,19 @@ const color = require('ansi-256-colors');
 const ROOT = 'logs';
 
 class Logging {
-  error(err) {
-    //const text = `(${err.code}) ${err.msg} ${err.dest ? err.dest : ''}\n ${err.stack}`;
-    this.Log('error', err, [2, 0, 0]);
+  static error(text) {
+    this.Log('error', text, [2, 0, 0]);
   }
 
-  info(text) {
+  static info(text) {
     this.Log('info', text, [0, 3, 0]);
   }
 
-  warn(err) {
-    //const text = `(${err.code}) ${err.msg} ${err.dest}\n ${err.stack}`;
-    this.Log('warn', err, [5, 3, 0]);
+  static warn(text) {
+    this.Log('warn', text, [5, 3, 0]);
   }
 
-  Log(label, text, colr) {
+  static Log(label, text, colr) {
     colr = colr || [5, 5, 5];
 
     const date = new Date()
